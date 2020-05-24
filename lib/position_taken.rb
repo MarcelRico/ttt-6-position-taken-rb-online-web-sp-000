e50890abcfc243f0ae2293c5c3a411917a5beb63
Marcel Rico <personal.mr4@gmail.com>
2020-05-24 16:04:09 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,5 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  potential_move = board[index]
-  puts potential_move
+  (board[index] == "X" || board[index] == "O") ? true : false
 end
